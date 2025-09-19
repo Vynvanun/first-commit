53fafcdd5b89997cedfbf6ea3d1419e98617f0c3
vynvanun <muhammmadgavyn@gmail.com>
2025-09-19 10:31:00 +0700

    belajar boolean

---
+++ 7_boolean.dart
@@ -0,0 +1,17 @@
+void main(List<String> args) {
+  /**
+   * simbol ! menandakan not atau buka
+   */
+  /**
+   * !i love u => not i love u
+   */
+  /**
+   * tipe data boolen hanya ada 2 nilai
+   * true and false
+   */
+  var isTrue = true;
+  print(isTrue); //true
+  print(!isTrue); //false
+  print(true != false); //true
+  print(false != false); 
+}
