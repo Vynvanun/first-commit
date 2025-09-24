42f61003bd31cd5431256497b7c21cf3afbb5988
vynvanun <muhammmadgavyn@gmail.com>
2025-09-24 07:43:43 +0700

    latihan totd

--- totd_tema.dart
+++ totd_tema.dart
@@ -2,5 +2,9 @@ import 'dart:io';
 
 void main(List<String> args) {
   //tentang kalkulator
+  stdout.write("halo selamat datang di \naplikasi kalkulator ");
+
   stdout.write("masukkan jari jari ");
+  var r =double.parse(stdin.readLineSync()!);
+
 }
