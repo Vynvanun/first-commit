3d965b4b0461c7bcadb927588a56743030b4163e
vynvanun <muhammmadgavyn@gmail.com>
2025-09-26 13:51:03 +0700

    latihan totd

--- totd_tema.dart
+++ totd_tema.dart
@@ -4,7 +4,10 @@ void main(List<String> args) {
   //tentang kalkulator
   stdout.write("halo selamat datang di \naplikasi kalkulator ");
 
-  stdout.write("masukkan jari jari ");
-  var r =double.parse(stdin.readLineSync()!);
+  stdout.write("masukkan angka 1 untuk mencari luas lingkaran dan 2 untuk keliling nya");
+  int typenya = int.parse(stdin.readLineSync()!);
+  if (int typenya = 1) {
+    
+  }
 
 }
