71b6013ea1096d7730cb97c2dedb402384a00bee
vynvanun <muhammmadgavyn@gmail.com>
2025-09-29 11:22:22 +0700

    lanjut kemarin

---
+++ 15_foooor_loop.dart
@@ -0,0 +1,27 @@
+void main(List<String> args) {
+  /**
+   * for loop digunakan untuk menjalankan perulangan kode
+   * yang mana jika sudah mencapai hasil yang diinginkan maka
+   * program akan berhenti
+   * kata kuncinya menggunakan for(kondisi){ output }
+   */
+
+  int iterasi =1; 
+  //i = iterasi;
+  //kita ingin melakukan perulangan angka dari 1-10
+  //kita bisa menggunakan for loop
+
+  //(variable; kondisi ; increment/decrement)
+
+  for (var i = 1; i <= 10; i--) {
+    print('ini adalah angka $i');
+
+
+    //pengecekan value dari variable
+    //value di cek dari kondisi <= 10
+    //jika benar makan print
+    //karena true maka lakuakan increment i++ (increment)
+    //balik lagi ke pengecekan value dari variable
+    //jika kondisi nilainya false maka stop program
+  }
+}
