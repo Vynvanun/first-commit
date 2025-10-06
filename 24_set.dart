d4e339f54627c1264fd7616b4fe13baa94af063e
vynvanun <muhammmadgavyn@gmail.com>
2025-10-06 13:11:21 +0700

    Belajar Set collection

--- 24_set.dart
+++ 24_set.dart
@@ -21,4 +21,32 @@ void main(List<String> args) {
     // dart juga mendukung union dn inersection
     //union +> menggabungkan 2 data atau lebih menjadi satu himpunan
     //intersection() => hanya menggabungkan "data yang sama" dari kedua himpunan yang dipertimbangkan
+
+    Set<int> segmentA = {1, 3, 5, 7, 9};
+    Set<int> segmentB = {2, 4, 6, 8, 10};
+
+    /**
+     * uNION => Gabungan dari 2 set / lebih
+     * 
+     */
+    var unionSegmen = segmentA.union(segmentB);
+    print(unionSegmen);
+
+    var inersection = segmentB.intersection(segmentB);
+    print(inersection);
+
+    Set<int> segmentC = {1, 5, 7, 9, 10};
+    var cekSama = segmentA.intersection(segmentC);
+    print(cekSama);
+
+    /**
+     * buatkan 2 set yang menampung nama nama minimal 7 nama
+     * dari masing maisng set
+     * 1. tampilkan unionnya
+     * 2. tampilkan intersectionnya
+     * 3. tentukan panjang element dari keduanya
+     * 4. tentukan panjang element dari unionnya
+     * 5. tentukan panjang element dari intersectionnya
+     */
+    
 }
